f644c22155e13745431358a6819457f9f55b1f5b
Zbyso <djkalicor@gmail.com>
2022-12-08 23:42:26 +0100

    Collision class added with OnCollisionEnter and OnTriggerEnter, added World sprites

---
+++ Assets/Scripts/Collision.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D other) {
+        Debug.Log(other);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("Triggered");
+    }
+}
